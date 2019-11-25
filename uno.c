e1badfdfb4758b21543003e8992e9382f97e1c4d
polmariin01 <45402851+polmariin01@users.noreply.github.com>
2019-11-25 15:47:04 +0100

    uno.c
    Programa gordo

---
+++ uno.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "extres.h"
+
+int main() {
+	intro();
+	printf("VAmo a ase la wea ");
+	float mango;
+	int wea;
+	scanf("%d",&wea);
+	mango=prova(wea);
+	printf("La wea %d del mango %f",wea, mango);
+}
