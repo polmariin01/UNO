d5b2ce63b2f0da7903e65b69b6d7746c4a954485
gilboleda <58255939+gilboleda@users.noreply.github.com>
2019-11-27 15:18:15 +0100

    Update carta.h

--- carta.h
+++ carta.h
@@ -1,26 +1,35 @@
-#include <stdio.h>
-
-#define MAXCART 108
-
+#define CARTA_H
 
 typedef struct
 {
-	int col;	//color (0 - sin color, 1 - azul, 2 - rojo, 3 - verde, 4 - amarillo)
-	int car;	//tipo de carta (0,1,2,3,4,5,6,7,8,9,10(S),11(R),12(+2),13(W),14(+4))
+	int col;
+	char car;
 } tcarta;
 
 typedef struct
 {
-	int n;			//numero de cartas en el mazo
-	tcarta mazo[MAXCART];	//cartas del mazo
+	int n;
+	tcarta mazo[MAXCART];
 } temazo;
 
 typedef struct
 {
-	char nom[20];	//nombre jugador
-	temazo c;	//mazo del jugador
+	char nom[20];
+	temazo c;
+	int uno; //0 si no te uno, 1 si te uno//
 } tjug;
+
+typedef struct
+{
+	int color;
+	int sentido; //0 horario, 1 antihorario//
+	int comprobacion; //0 cubert, 1 descubert//
+}tpartida;
 
 
 void mostrar_carta(tcarta carta);
 void comparar_carta(tcarta carta1, tcarta carta2);
+
+
+
+#endif
