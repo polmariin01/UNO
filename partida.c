bc1f92f2078fba2d9181dd77e72b5966e0d7911d
polmariin01 <45402851+polmariin01@users.noreply.github.com>
2019-11-28 15:57:57 +0100

    Update partida.c

--- partida.c
+++ partida.c
@@ -27,13 +27,7 @@ int cambio_turno(int turno, int jugadores, int sentido)
 	}	
 return turno;
 }
-int jugadas(tcarta a, temazo b)
-{
-	for(i=0; i<b.mazo[MAXCART]; i++)
-	{
-		if (b.cartas.num==a.num || b.color== a.color)
-		{ //preguntar al gil i acabar codi//
-}
+
 tpartida inicio()
 {
 	tpartida partida;
@@ -50,12 +44,5 @@ tpartida inicio()
 		scanf ("%c", &tjug.nom[i]); 
 		i++;			
 	}while (tjug.nom!='\n');
-int uno()				//La variable que ha d'entrar es la del resultat de la 						funcio del gil: es_la_ultima, si el resultat de la funcio 						del gil es !=0 no uno.
-{
-	if (es_la_ultima==1)
-	{
-		printf("UNO")
-	}
-}
-						 
+}							 
 // ronda();
