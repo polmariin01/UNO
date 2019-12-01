0c7031cf48bb4076db9761d2464fe7ce5a1c1c40
gilboleda <58255939+gilboleda@users.noreply.github.com>
2019-12-01 15:56:54 +0100

    Update carta.c

--- carta.c
+++ carta.c
@@ -3,7 +3,7 @@
 
 void mostrar_carta(tcarta carta)
 {
-	cambiar_color_fondo(carta.color);
+	cambiar_color_fondo(carta.col);
 	switch (carta.num)
 	{
 		case 10:
@@ -25,17 +25,21 @@ void mostrar_carta(tcarta carta)
 			printf(" %d ", carta.num);
 			break;
 	}
-	cambiar_color_fondo(5);
-}	
+	default_attributes();
+}
 
-int comparar_carta(tcarta carta1; tcarta carta2);
-{
+int comparar_carta(tcarta carta1, tcarta carta2)
+{	
 	//carta 1 es la de sobre, si la jugada es posible = 1 sino =0//
 	int a=0;
-	if((carta1.color==carta2.color)||(carta1.num==carta2.num)||(carta2.num>=13))
+	if((carta1.col==carta2.col)||(carta1.num==carta2.num)||(carta2.num>=13))
+		{
 		a=1;
+		}
 	return(a);
 }
+
+
 
 
 
